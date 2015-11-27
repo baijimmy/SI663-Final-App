d00aa3e02ae0098f3cea4b482fd885d6c7829ceb
Jim Bai <bai.jimmy@hotmail.com>
2015-11-27 13:19:54 -0500

    Initial Commit

---
+++ Campus Genie appTests/Campus_Genie_appTests.swift
@@ -0,0 +1,36 @@
+//
+//  Campus_Genie_appTests.swift
+//  Campus Genie appTests
+//
+//  Created by Haoyang Bai on 11/27/15.
+//  Copyright © 2015 umsi363w15. All rights reserved.
+//
+
+import XCTest
+@testable import Campus_Genie_app
+
+class Campus_Genie_appTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
